f0c5bf0dd7c8f5875d1902c32fca553fee66058a
fanchao <van.chao@hotmail.com>
2017-06-28 10:23:20 +0800

    This is a deep more step to custom this project

---
+++ config/config_example.php
@@ -0,0 +1,16 @@
+<?php
+// DEBUG message
+define('DEBUG', true);
+
+// Database connetct info
+define('DB_NAME', 'dbname');
+define('DB_USER', 'dbuser');
+define('DB_PASSWORD', 'dbpass');
+define('DB_HOST', 'dbhost');
+
+// website url and path
+define('PATH', 'http://www.yoursite.com');
+define('WEBSITE_TITLE', 'youresite.com');
+
+// make default controller to load
+define('WEBSITE_TITLE', 'youresite.com');
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Hi! I am bootstrap!";
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'core' . DS . 'functions.php');
+
+function __autoload($className) {
+    if (file_exists(ROOT . DS . 'core' . DS . strtolower($className) . '.php')) {
+        require_once(ROOT . DS . 'core' . DS . strtolower($className) . '.php');
+    } else if (file_exists(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php')) {
+        require_once(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php');
+    } else if(file_exists(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php');
+    } else if(file_exists(ROOT . DS . 'core' . DS . 'sql.php')) {
+        require_once(ROOT . DS . 'core' . DS . 'sql.php');
+    }
+    
+}
